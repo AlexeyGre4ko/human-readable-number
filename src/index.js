1378c981cfbea601c804f81c73adac10008a190a
AlexeyGre4ko <alexeygre4ko@gmail.com>
2022-08-04 21:59:59 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+ let dg = ['zero','one','two','three','four','five','six','seven','eight','nine'] 
+ let tn = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+ let tw = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+ let th = 'hundred'
+
+ if (number==0)
 }
